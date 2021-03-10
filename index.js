8f2da026ec6b95eef87cb60bc4b17c7c917e2813
StepArtKazan <80350818+StepArtKazan@users.noreply.github.com>
2021-03-10 22:18:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,103 +1,99 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 
-// const url = 'https://www.amazon.com/s?k=stand+up+desks&ref=nb_sb_noss';
-//const port = 3005;
+const url = 'https://www.amazon.com/s?k=stand+up+desks&ref=nb_sb_noss';
+const port = 3002;
 
-app.get("/", (req, res) => {
-  res.send("Hello Word")
-})
-app.listen(80)
-//
-// app.get('/', async (req, res) => {
-//     console.log('get parse info');
-//     const result = await fetchProductList(url);
-//
-//     res.send(JSON.stringify(result));
-// });
-//
-// app.listen(port);
-//
-// async function fetchProductList(url) {
-//     let startTime = Date.now();
-//
-//     const browser = await puppeteer.launch({
-//         headless: true,
-//         defaultViewport: null
-//     });
-//
-//     const page = await browser.newPage();
-//     // await page.setViewport({width: 1366, height: 800});
-//     await page.goto(url, {waitUntil: 'networkidle2'});
-//
-//     // await page.waitForSelector('div.s-desktop-width-max');
-//     await page.waitFor('div[data-cel-widget^="search_result_"]');
-//
-//     const result = await page.evaluate(() => {
-//         // counts total number of products
-//         let totalSearchResults = Array.from(document.querySelectorAll('div[data-cel-widget^="search_result_"]')).length;
-//
-//         let productsList = [];
-//
-//         for (let i = 1; i < totalSearchResults - 1; i++) {
-//             let product = {
-//                 brand: '',
-//                 product: '',
-//             };
-//             let onlyProduct = false;
-//             let emptyProductMeta = false;
-//
-//             // traverse for brand and product names
-//             let productNodes = Array.from(document.querySelectorAll(`div[data-cel-widget="search_result_${i}"] .a-size-base-plus.a-color-base`));
-//
-//             if (productNodes.length === 0) {
-//                 // traverse for brand and product names
-//                 // (in case previous traversal returned empty elements)
-//                 productNodes = Array.from(document.querySelectorAll(`div[data-cel-widget="search_result_${i}"] .a-size-base-plus.a-color-base.a-text-normal`));
-//                 productNodes.length > 0 ? onlyProduct = true : emptyProductMeta = true;
-//             }
-//
-//             let productsDetails = productNodes.map(el => el.innerText);
-//
-//             if (!emptyProductMeta) {
-//                 product.brand = onlyProduct ? '' : productsDetails[0];
-//                 product.product = onlyProduct ? productsDetails[0] : productsDetails[0];
-//             }
-//
-//             // traverse for product image
-//             let rawImage = document.querySelector(`div[data-cel-widget="search_result_${i}"] .s-image`);
-//             product.image = rawImage ? rawImage.src : '';
-//
-//             // traverse for product url
-//             let rawUrl = document.querySelector(`div[data-cel-widget="search_result_${i}"] a.a-text-normal`);
-//             product.url = rawUrl ? rawUrl.href : '';
-//
-//             // traverse for product price
-//             let rawPrice = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-offscreen`);
-//             product.price = rawPrice ? rawPrice.innerText : '';
-//
-//             let rawRating = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-icon-alt`);
-//             product.rating = rawRating ? rawRating.innerText : '';
-//
-//             let rawFeedback = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-size-base`);
-//             product.Feedback = rawFeedback ? rawFeedback.innerText : '';
-//
-//             if (typeof product.product !== 'undefined') {
-//                 !product.product.trim() ? null : productsList = productsList.concat(product);
-//             }
-//         }
-//
-//         return {
-//             productsList: productsList,
-//             totalLength: totalSearchResults
-//         };
-//     })
-//
-//     console.log(result, ' result');
-//
-//     return result
-//
-// }
-//
-// //fetchProductList(url)
+
+app.get('/', async (req, res) => {
+    console.log('get parse info');
+    const result = await fetchProductList(url);
+
+    res.send(JSON.stringify(result));
+});
+
+app.listen(port);
+
+async function fetchProductList(url) {
+    let startTime = Date.now();
+
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null
+    });
+
+    const page = await browser.newPage();
+    // await page.setViewport({width: 1366, height: 800});
+    await page.goto(url, {waitUntil: 'networkidle2'});
+
+    // await page.waitForSelector('div.s-desktop-width-max');
+    await page.waitFor('div[data-cel-widget^="search_result_"]');
+
+    const result = await page.evaluate(() => {
+        // counts total number of products
+        let totalSearchResults = Array.from(document.querySelectorAll('div[data-cel-widget^="search_result_"]')).length;
+
+        let productsList = [];
+
+        for (let i = 1; i < totalSearchResults - 1; i++) {
+            let product = {
+                brand: '',
+                product: '',
+            };
+            let onlyProduct = false;
+            let emptyProductMeta = false;
+
+            // traverse for brand and product names
+            let productNodes = Array.from(document.querySelectorAll(`div[data-cel-widget="search_result_${i}"] .a-size-base-plus.a-color-base`));
+
+            if (productNodes.length === 0) {
+                // traverse for brand and product names
+                // (in case previous traversal returned empty elements)
+                productNodes = Array.from(document.querySelectorAll(`div[data-cel-widget="search_result_${i}"] .a-size-base-plus.a-color-base.a-text-normal`));
+                productNodes.length > 0 ? onlyProduct = true : emptyProductMeta = true;
+            }
+
+            let productsDetails = productNodes.map(el => el.innerText);
+
+            if (!emptyProductMeta) {
+                product.brand = onlyProduct ? '' : productsDetails[0];
+                product.product = onlyProduct ? productsDetails[0] : productsDetails[0];
+            }
+
+            // traverse for product image
+            let rawImage = document.querySelector(`div[data-cel-widget="search_result_${i}"] .s-image`);
+            product.image = rawImage ? rawImage.src : '';
+
+            // traverse for product url
+            let rawUrl = document.querySelector(`div[data-cel-widget="search_result_${i}"] a.a-text-normal`);
+            product.url = rawUrl ? rawUrl.href : '';
+
+            // traverse for product price
+            let rawPrice = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-offscreen`);
+            product.price = rawPrice ? rawPrice.innerText : '';
+
+            let rawRating = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-icon-alt`);
+            product.rating = rawRating ? rawRating.innerText : '';
+
+            let rawFeedback = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-size-base`);
+            product.Feedback = rawFeedback ? rawFeedback.innerText : '';
+
+            if (typeof product.product !== 'undefined') {
+                !product.product.trim() ? null : productsList = productsList.concat(product);
+            }
+        }
+
+        return {
+            productsList: productsList,
+            totalLength: totalSearchResults
+        };
+    })
+
+    console.log(result, ' result');
+
+    return result
+
+}
+
+//fetchProductList(url)
